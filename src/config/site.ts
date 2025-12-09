91eb257f94f10903f75ffc4c9fed97cef719c9c8
Twan Meurs <tmeurs@student.che.nl>
2025-12-09 15:24:39 +0100

    new spot

--- src/config/site.ts
+++ src/config/site.ts
@@ -49,7 +49,7 @@ export const embeds = {
    * https://open.spotify.com/embed/playlist/37i9dQZF1DX48TTZL62Yht?utm_source=generator&theme=0
    */
   spotifyEmbedSrc:
-    "https://open.spotify.com/embed/playlist/49CdfN106d7t5EbnKlhGtF?utm_source=generator&theme=0" // <— bewerkbaar
+    "https://open.spotify.com/embed/playlist/1poeapOfDehRZlPwyl0T8q?si=dUO7la7zSMuEjXCdDgVmQA&pi=ohHxkpG3Ri2ep ?utm_source=generator&theme=0" // <— bewerkbaar
 };
 
 export const analytics = {
